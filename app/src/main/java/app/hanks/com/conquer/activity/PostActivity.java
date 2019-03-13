b15bd5a1c82cbd4388be1c67e883d456dba38b49
Wiki <zhengweiqunemail@qq.com>
2019-03-13 11:26:59 +0800

    feat: 添加日历和时间dialog逻辑

--- app/src/main/java/app/hanks/com/conquer/activity/PostActivity.java
+++ app/src/main/java/app/hanks/com/conquer/activity/PostActivity.java
@@ -2,12 +2,22 @@ package app.hanks.com.conquer.activity;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import app.hanks.com.conquer.R;
 import app.hanks.com.conquer.base.BaseActivity;
 import app.hanks.com.conquer.bean.TodoBean;
 import app.hanks.com.conquer.util.L;
 import app.hanks.com.conquer.util.T;
+import app.hanks.com.conquer.view.datetime.datepicker.DatePickerDialog;
+import app.hanks.com.conquer.view.datetime.timepicker.RadialPickerLayout;
+import app.hanks.com.conquer.view.datetime.timepicker.TimePickerDialog;
 import cn.bmob.v3.listener.SaveListener;
 
 /**
@@ -16,8 +26,12 @@ import cn.bmob.v3.listener.SaveListener;
  */
 public class PostActivity extends BaseActivity implements View.OnClickListener {
 
+    private final Calendar mCalendar = Calendar.getInstance();
+    private TimePickerDialog timePickerDialog24h;
+    private DatePickerDialog datePickerDialog;
     //---UIView
-    private EditText inputEt;
+    private EditText etInput;
+    private TextView tvDateTime;
 
     @Override
     protected void initView() {
@@ -27,7 +41,10 @@ public class PostActivity extends BaseActivity implements View.OnClickListener {
         findViewById(R.id.btnTypeList).setOnClickListener(this);
         findViewById(R.id.btnPriority).setOnClickListener(this);
         findViewById(R.id.btnOk).setOnClickListener(this);
-        inputEt = (EditText) findViewById(R.id.etInput);
+        etInput = (EditText) findViewById(R.id.etInput);
+        tvDateTime = (TextView) findViewById(R.id.tvDateTime);
+
+        initDatePicker();
     }
 
     @Override
@@ -36,11 +53,12 @@ public class PostActivity extends BaseActivity implements View.OnClickListener {
             case R.id.btnBack:
                 finish();
                 break;
-            case R.id.btnChooseCalendar:
+            case R.id.btnChooseCalendar://日期
+                datePickerDialog.show(getFragmentManager(), "");
                 break;
-            case R.id.btnTypeList:
+            case R.id.btnTypeList://类型
                 break;
-            case R.id.btnPriority:
+            case R.id.btnPriority://优先级
                 break;
             case R.id.btnOk:
                 TodoBean bean = new TodoBean(1222, "", "3.15日，中期检查",
@@ -61,4 +79,59 @@ public class PostActivity extends BaseActivity implements View.OnClickListener {
                 break;
         }
     }
+
+    /**
+     * 初始化日历时间的选择控件
+     */
+    private void initDatePicker() {
+        mCalendar.add(Calendar.MINUTE, 60);// 设成60分钟后
+//        setTimeAndTip(new SimpleDateFormat("yyyy/MM/dd", Locale.CHINA).format(mCalendar.getTime()) + " "
+//                + pad(mCalendar.get(Calendar.HOUR_OF_DAY)) + ":" + pad(mCalendar.get(Calendar.MINUTE)));
+
+        timePickerDialog24h = TimePickerDialog.newInstance(new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+//                tvDateTime.setText(new StringBuffer().append(tvDateTime.getText()).append(" ").
+//                        append(pad(hourOfDay)).append(":").append(pad(minute)));
+                setTimeAndTip(tvDateTime.getText() + " " + pad(hourOfDay) + ":" + pad(minute));
+            }
+        }, mCalendar.get(Calendar.HOUR_OF_DAY), mCalendar.get(Calendar.MINUTE), true);
+
+        datePickerDialog = DatePickerDialog.newInstance(new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+                Calendar c = Calendar.getInstance();
+                c.set(year, month, day);
+                tvDateTime.setText(new StringBuilder().append(pad(year)).append("/").append(pad(month + 1))
+                        .append("/").append(pad(day)));
+                timePickerDialog24h.show(getFragmentManager(), "");
+            }
+        }, mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DAY_OF_MONTH));
+        int curYear = mCalendar.get(Calendar.YEAR);
+        datePickerDialog.setYearRange(curYear, mCalendar.get(Calendar.MONTH) >= 11 ? curYear + 1 : curYear);
+    }
+
+    private String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+
+    /**
+     * 设置tv_time 和 tv_time_tip的文本内容
+     *
+     * @param string 类似yyyy/MM/dd HH:mm
+     */
+    private void setTimeAndTip(String string) {
+        tvDateTime.setText(string);
+//        tvDateTime.setText(string.substring(string.length() - 5, string.length()));
+        try {
+            Date d = new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.CHINA).parse(string);
+//            tv_time_tip.setTextColor(d.getTime() > System.currentTimeMillis() ? Color.GRAY : Color.RED);
+//            tv_time_tip.setText("(" + TaskUtil.getDescriptionTimeFromTimestamp(d.getTime()) + ")");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
